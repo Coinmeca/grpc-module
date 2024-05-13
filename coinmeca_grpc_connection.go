ab0f4f0e6d39fca6d3364552fca6e61cf14f109f
Giga <gidae7054@coinmeca.net>
2024-05-14 03:06:12 +0900

    Add: grpc server v2

--- coinmeca_grpc_connection.go
+++ coinmeca_grpc_connection.go
@@ -4,6 +4,9 @@ import (
 	context "context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -32,6 +35,35 @@ func InitGrpcClient(address string) (CoinmecaGrpcModuleClient, *grpc.ClientConn)
 	}
 	client := NewCoinmecaGrpcModuleClient(conn)
 	return client, conn
+}
+
+func StartGrpcServerV2(grpcCallBack GrpcCallBack, port string) {
+	go func() {
+		lis, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Printf("Failed to listen: %v", err)
+			return
+		}
+		defer lis.Close()
+
+		s := grpc.NewServer()
+		RegisterCoinmecaGrpcModuleServer(s, NewServer(grpcCallBack))
+
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+		go func() {
+			<-stop
+			log.Println("Shutting down gRPC server...")
+			s.GracefulStop()
+			lis.Close()
+		}()
+
+		log.Printf("gRPC server listening at %v", lis.Addr())
+		if err := s.Serve(lis); err != nil {
+			log.Printf("Failed to serve: %v", err) // 종료하지 않고 오류 로깅
+		}
+	}()
 }
 
 func StartGrpcServer(grpcCallBack GrpcCallBack, port string) {
